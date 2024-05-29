ba29f0cd8ee1c375701ce21ec8d2f535edf47672
Gideon Adeti <gideonadeti0@gmail.com>
2024-05-29 04:54:30 +0000

    Implement capitalize test and it's function

---
+++ capitalize.test.js
@@ -0,0 +1,9 @@
+import capitalize from "./capitalize";
+
+test("capitalizes the first character of a string", () => {
+  expect(capitalize("hello")).toBe("Hello");
+  expect(capitalize("world")).toBe("World");
+  expect(capitalize("javaScript")).toBe("JavaScript");
+  expect(capitalize("")).toBe("");
+  expect(capitalize("a")).toBe("A");
+});
