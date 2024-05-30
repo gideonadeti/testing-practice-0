b4de7cb860e517b63164382790c355209c7306ce
Gideon Adeti <gideonadeti0@gmail.com>
2024-05-30 04:22:40 +0000

    Implement reverString test and it's function

---
+++ reverseString.test.js
@@ -0,0 +1,8 @@
+import reverseString from './reverseString'
+
+test("reverses a string", () => {
+  expect(reverseString("hello")).toBe("olleh");
+  expect(reverseString("world")).toBe("dlrow");
+  expect(reverseString("")).toBe("");
+  expect(reverseString("a")).toBe("a");
+});
