62b89e45581b9d41de577eff687419cde735e60d
Gideon Adeti <gideonadeti0@gmail.com>
2024-05-30 04:43:24 +0000

    Implement analyzeArray test and it's function

---
+++ analyzeArray.js
@@ -0,0 +1,9 @@
+export default function analyzeArray(arr) {
+  const sum = arr.reduce((acc, num) => acc + num, 0);
+  const average = sum / arr.length;
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const length = arr.length;
+
+  return { average, min, max, length };
+}
